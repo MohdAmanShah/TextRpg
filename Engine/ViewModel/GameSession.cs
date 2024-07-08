da5951b663b97194827763a040446230134fcef0
mohdamanshah <reach.mohdamanshah@gmail.com>
2024-07-08 16:59:27 +0530

    added images and built models for player and location

---
+++ Engine/ViewModel/GameSession.cs
@@ -0,0 +1,35 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModel
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player()
+            {
+                Name = "Noa",
+                Gold = 10,
+                CharacterClass = "Mage",
+                Level = 1,
+                ExperiencePoints = 0,
+                HitPoints = 100
+            };
+            CurrentLocation = new Location()
+            {
+                XCoordinate = 0,
+                YCoordinate = -1,
+                Name = "Home",
+                Description = "This is your house.",
+                ImageName = $"/Engine;component/Images/Locations/Home.png"
+            };
+        }
+    }
+}
--- TextRpg/MainWindow.xaml.cs
+++ TextRpg/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using Engine.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TextRpg
 {
@@ -16,9 +8,16 @@ namespace TextRpg
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GameSession _session;
         public MainWindow()
         {
             InitializeComponent();
+            _session = new GameSession();
+            DataContext = _session;
+        }
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _session.CurrentPlayer.ExperiencePoints += 10;
         }
     }
 }
